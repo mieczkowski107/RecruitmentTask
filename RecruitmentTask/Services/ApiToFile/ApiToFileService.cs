8043395941a4a01f98a707371b9a8731aaf8935b
lmieczkowski <mieczkowski107@gmail.com>
2025-07-20 13:24:15 +0200

    feat: implement service to fetch cat fact and save it to file

---
+++ RecruitmentTask/Services/ApiToFile/ApiToFileService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using RecruitmentTask.Models;
+using RecruitmentTask.Services.ApiHandler;
+using RecruitmentTask.Services.FileHandler;
+
+
+namespace RecruitmentTask.Services.ApiToFile;
+
+public class ApiToFileService : IApiToFileService
+{
+    private readonly IApiHandler _apiHandler;
+    private readonly IFileHandler _fileHandler;
+    private readonly FileSetting _fileSetting;
+    public ApiToFileService(IApiHandler apiHandler, IFileHandler fileHandler, IOptions<FileSetting> fileSetting)
+    {
+        _apiHandler = apiHandler;
+        _fileHandler = fileHandler;
+        _fileSetting = fileSetting.Value;
+    }
+
+    public async Task<CatFact?> FetchAndSave()
+    {
+        var catFact = await _apiHandler.GetCatFact();   
+        if (catFact != null)
+        {
+            var content = FormatCatFactForFile(catFact);
+            var filePath = Path.Combine(_fileSetting.Path, _fileSetting.Name);
+            await _fileHandler.AppendToFile(filePath, content);
+            return catFact;
+        }
+        return null;
+    }
+
+    private string FormatCatFactForFile(CatFact catFact)
+    {
+        return $"{catFact.Fact};{catFact.Length}\n";
+    }
+}
---
+++ RecruitmentTask/Services/ApiToFile/IApiToFileService.cs
@@ -0,0 +1,8 @@
+﻿using RecruitmentTask.Models;
+
+namespace RecruitmentTask.Services.ApiToFile;
+
+public interface IApiToFileService
+{
+    Task<CatFact?> FetchAndSave();
+}
