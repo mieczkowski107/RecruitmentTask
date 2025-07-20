4656f694c7a94a8d8ef3ecaaf3f700c0a1d7fb4e
lmieczkowski <mieczkowski107@gmail.com>
2025-07-20 13:25:25 +0200

    feat: register DI services and bind FileSetting from appsettings.json

--- RecruitmentTask/Program.cs
+++ RecruitmentTask/Program.cs
@@ -1,36 +1,50 @@
-namespace RecruitmentTask
+using Microsoft.Extensions.Options;
+using RecruitmentTask.Services.ApiHandler;
+using RecruitmentTask.Services.ApiToFile;
+using RecruitmentTask.Services.FileHandler;
+
+namespace RecruitmentTask;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+
+        builder.Services.Configure<FileSetting>(builder.Configuration.GetSection("FileSetting"));
+        builder.Services.AddHttpClient<IApiHandler, ApiHandler>((httpClient) =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            httpClient.BaseAddress = new Uri("https://catfact.ninja");
+        });
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+        builder.Services.AddScoped<IFileHandler, FileHandler>();
+        builder.Services.AddScoped<IApiToFileService, ApiToFileService>();
 
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
