a26180a464c5e7ac91e85399d797c64553e4c0a2
lmieczkowski <mieczkowski107@gmail.com>
2025-07-20 14:22:42 +0200

    refactor: update HomeController to use CatFactService and FileDto - IApiToFileService to ICatFactService after renaming - change CatFactFileDownload  to use GetCatFile() from service and fileDto - removed unused FileSetting

--- RecruitmentTask/Controllers/HomeController.cs
+++ RecruitmentTask/Controllers/HomeController.cs
@@ -1,41 +1,36 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using RecruitmentTask.Models;
-using RecruitmentTask.Services.ApiToFile;
+using RecruitmentTask.Services.CatFactService;
+using System.Diagnostics;
 
-namespace RecruitmentTask.Controllers
+namespace RecruitmentTask.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+    private readonly ICatFactService _catFactService;
+
+    public HomeController(ILogger<HomeController> logger, ICatFactService apiToFileService)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IApiToFileService _apiToFileService;
-        private readonly FileSetting _fileSetting;
-       
-        public HomeController(ILogger<HomeController> logger, IApiToFileService apiToFileService, IOptions<FileSetting> fileSetting)
-        {
-            _logger = logger;
-            _apiToFileService = apiToFileService;
-            _fileSetting = fileSetting.Value;
-            
-        }
+        _logger = logger;
+        _catFactService = apiToFileService;
+    }
 
-        public async Task<IActionResult> Index()
-        {
-            var fetchedCatFact = await _apiToFileService.FetchAndSave();
-            return View(fetchedCatFact);
-        }
+    public async Task<IActionResult> Index()
+    {
+        var fetchedCatFact = await _catFactService.FetchAndSave();
+        return View(fetchedCatFact);
+    }
 
-        public IActionResult CatFactFileDownload()
-        {
-            var filepath = Path.Combine(_fileSetting.Path, _fileSetting.Name);
-            return File(System.IO.File.ReadAllBytes(filepath),"text/plain", _fileSetting.Name.ToLower());
-        }
+    public async Task<IActionResult> CatFactFileDownload()
+    {
+        var file = await _catFactService.GetCatFile();
+        return File(file.Data, file.MimeType, file.Name);
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
